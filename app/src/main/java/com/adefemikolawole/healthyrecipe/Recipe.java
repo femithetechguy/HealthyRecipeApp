7e51bac9ceac7b92a1f04f8e546df1749b7d6453
Adefemi Kolawole <adefemikolawole@gmail.com>
2018-09-04 13:33:06 -0400

    added new files to the functioning app that we have according to specification

--- app/src/main/java/com/adefemikolawole/healthyrecipe/Recipe.java
+++ app/src/main/java/com/adefemikolawole/healthyrecipe/Recipe.java
@@ -9,5 +9,6 @@ public class Recipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
     }
 }
