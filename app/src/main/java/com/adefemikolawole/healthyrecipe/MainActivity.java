993a8eaf2ef8a4b84124405c43f2cce2c7f4759a
Adefemi Kolawole <adefemikolawole@gmail.com>
2018-09-04 13:30:21 -0400

    added intent to MainActivity , BUtton click click on this page starts the Recipe page/ activity

--- app/src/main/java/com/adefemikolawole/healthyrecipe/MainActivity.java
+++ app/src/main/java/com/adefemikolawole/healthyrecipe/MainActivity.java
@@ -1,13 +1,29 @@
 package com.adefemikolawole.healthyrecipe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btnRecipe);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRecipe();
+            }
+        });
+
+    }
+
+    public void openRecipe(){
+        Intent intent = new Intent (this, Recipe.class);
+startActivity(intent);
     }
 }
